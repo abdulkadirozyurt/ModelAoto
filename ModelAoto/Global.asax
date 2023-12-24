59e40ea6ca425567403deeecb34a637b3b72e2b7
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-12-24 17:34:32 +0300

    Add project files.

---
+++ ModelAoto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ModelAoto.MvcApplication" Language="C#" %>
---
+++ ModelAoto/Models/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelAoto.Models.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int StockAmount { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal SalePrice { get; set; }
+        public bool Status { get; set; }
+        public string Image { get; set; }
+
+
+
+
+
+
+    }
+}
