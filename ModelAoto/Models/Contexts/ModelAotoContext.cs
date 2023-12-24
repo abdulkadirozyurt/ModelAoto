144eeb557cdf9ddb3e7ffc92b22e34e4aa505a94
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-12-24 20:33:40 +0300

    Connection string added to Web.Config file

---
+++ ModelAoto/Models/Contexts/ModelAotoContext.cs
@@ -0,0 +1,27 @@
+﻿using ModelAoto.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ModelAoto.Models.Contexts
+{
+    public class ModelAotoContext : DbContext
+    {
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<EmployeeDepartment> EmployeeDepartments { get; set; }
+        public DbSet<InvoiceDetail> InvoiceDetails { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+
+
+
+    }
+}
