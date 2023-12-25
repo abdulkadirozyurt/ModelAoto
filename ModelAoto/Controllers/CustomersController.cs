28aff629975cffe99d3ca37adc200907a36b2f50
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-12-25 21:51:34 +0300

    customers add page created

---
+++ ModelAoto/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using ModelAoto.Models.Contexts;
+using ModelAoto.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelAoto.Controllers
+{
+    public class CustomersController : Controller
+    {
+        ModelAotoDbContext db = new ModelAotoDbContext();
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = db.Customers.ToList();
+            return View(customers);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Customer customer)
+        {
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
